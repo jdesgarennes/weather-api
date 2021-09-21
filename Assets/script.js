e4427c2f20cd4d5030bb30472342fd1cd1e0bbe7
jdesgarennes <johndesgarennes@gmail.com>
2021-09-21 10:51:56 -0700

    Made base htm files and scripts

---
+++ Assets/script.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+var myClick = document.querySelector('#buttonInput');
+var myInput = document.querySelector('#textInput');
+
+
+
+function createStorage (){
+var inputvalue = 
+  localStorage.setItem(myInput.value);
+
+}
+
+createStorage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+myInput.addEventListener('click'.)
