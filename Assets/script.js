9c1548a3686a49cb56f97c2b208e03a299b31ad3
jdesgarennes <johndesgarennes@gmail.com>
2021-09-22 10:35:41 -0700

    added and tested fetch API

--- Assets/script.js
+++ Assets/script.js
@@ -1,35 +1,40 @@
+var searchCity = document.querySelector("#sButton");
+var cityInput = document.querySelector("#textBox");
+var apiKey = '1324b076d7d8ed0b45bf5b20fdc67fb8';
+var city='Murrieta';
 
 
 
 
+// Create fetch for API to get weather
+function getWeather(DATA) {
+  var city = cityInput.value;
+  var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q=';
 
-var myClick = document.querySelector('#buttonInput');
-var myInput = document.querySelector('#textInput');
-
-
-
-function createStorage (){
-var inputvalue = 
-  localStorage.setItem(myInput.value);
-
+  fetch(requestUrl + city +"&appid="+ apiKey+"&units=imperial", {
+})  
+.then(response => {
+ return response.json();
+})
+.then (data =>{
+  console.log(data)
+})
+.catch(err => {
+console.error(err);
+});
+return DATA;
 }
 
-createStorage();
 
 
 
 
 
+//getWeather();
 
 
 
+searchCity.addEventListener('click', function(){
+  getWeather();
+});
 
-
-
-
-
-
-
-
-
-myInput.addEventListener('click'.)
