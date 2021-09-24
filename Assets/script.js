f730212674b8bee3e626953dae4c0adbbe28b9b4
jdesgarennes <johndesgarennes@gmail.com>
2021-09-23 20:31:48 -0700

    added 5 day weather fetch, tests good.

--- Assets/script.js
+++ Assets/script.js
@@ -47,15 +47,28 @@ cHumidity.innerText = "Humidity: " + cityHumidity;
 
   });
 
+}
 
+function getWeatherFive(data5){
 
+    var requestUrl5 ='https://api.openweathermap.org/data/2.5/forecast?q=';
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=1324b076d7d8ed0b45bf5b20fdc67fb8', { 
+    }) 
+  .then(response => {
+   return response.json();
+  })
+  .then(function (data5){
+  console.log(data5);
+  cityName5= data5.name;
+  cityTemp=data5.main.temp;
+  cityHumidity=data5.main.humidity;
+  cityWind=data5.wind.speed;
+  cityIcon=data5.weather[0].icon;
 
+});
 
 
 }
-
-
-
 
 
 
@@ -65,6 +78,8 @@ cHumidity.innerText = "Humidity: " + cityHumidity;
 
 searchCity.addEventListener('click', function(){
   getWeather();
+  getWeatherFive();
+
 });
 
 
